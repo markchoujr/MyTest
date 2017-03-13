20e885d0d90f5cd5e650116854e31cc998716184
markchoujr <markchoujr@gmail.com>
2017-03-14 00:27:04 +0800

    增加Form1表單

---
+++ Solution1/Example03/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Example03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.product1Button = new System.Windows.Forms.Button();
+            this.product2Bbutton = new System.Windows.Forms.Button();
+            this.customerLabel = new System.Windows.Forms.Label();
+            this.orderRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // product1Button
+            // 
+            this.product1Button.Location = new System.Drawing.Point(38, 120);
+            this.product1Button.Name = "product1Button";
+            this.product1Button.Size = new System.Drawing.Size(105, 45);
+            this.product1Button.TabIndex = 0;
+            this.product1Button.Text = "button1";
+            this.product1Button.UseVisualStyleBackColor = true;
+            // 
+            // product2Bbutton
+            // 
+            this.product2Bbutton.Location = new System.Drawing.Point(177, 120);
+            this.product2Bbutton.Name = "product2Bbutton";
+            this.product2Bbutton.Size = new System.Drawing.Size(102, 45);
+            this.product2Bbutton.TabIndex = 1;
+            this.product2Bbutton.Text = "button2";
+            this.product2Bbutton.UseVisualStyleBackColor = true;
+            // 
+            // customerLabel
+            // 
+            this.customerLabel.AutoSize = true;
+            this.customerLabel.Location = new System.Drawing.Point(41, 40);
+            this.customerLabel.Name = "customerLabel";
+            this.customerLabel.Size = new System.Drawing.Size(33, 12);
+            this.customerLabel.TabIndex = 2;
+            this.customerLabel.Text = "label1";
+            // 
+            // orderRichTextBox
+            // 
+            this.orderRichTextBox.Location = new System.Drawing.Point(41, 193);
+            this.orderRichTextBox.Name = "orderRichTextBox";
+            this.orderRichTextBox.Size = new System.Drawing.Size(433, 233);
+            this.orderRichTextBox.TabIndex = 3;
+            this.orderRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 477);
+            this.Controls.Add(this.orderRichTextBox);
+            this.Controls.Add(this.customerLabel);
+            this.Controls.Add(this.product2Bbutton);
+            this.Controls.Add(this.product1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button product1Button;
+        private System.Windows.Forms.Button product2Bbutton;
+        private System.Windows.Forms.Label customerLabel;
+        private System.Windows.Forms.RichTextBox orderRichTextBox;
+    }
+}
+
